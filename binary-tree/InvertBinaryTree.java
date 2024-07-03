ab8aeb7bb593f398ac217049affbbaa644141b93
Cattherinee <klimanovakateryna@gmail.com>
2024-07-03 12:46:02 +0300

    Add binary tree problems

---
+++ binary-tree/InvertBinaryTree.java
@@ -0,0 +1,41 @@
+public class InvertBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
+
+
+// Invert Binary Tree
+// Visit a node, take a look at its two children, and swipe the positions of
+// them
+// Recursively run the invert tree on the left subtree and then on right subtree
+// Use Depth First Search
