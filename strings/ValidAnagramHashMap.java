891fea17410069bab85dfdc9841b13d3c2a06f02
Cattherinee <klimanovakateryna@gmail.com>
2024-07-03 14:20:50 +0300

    Add a function that checks if two words are anagrams using hasmaps

---
+++ strings/ValidAnagramHashMap.java
@@ -0,0 +1,40 @@
+package strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagramHashMap {
+
+    public static void main(String[] args) {
+        String testString1 = "anagram";
+        String testString2 = "nagaram";
+        String testString3 = "car";
+
+        System.out.println(isAnagram(testString1, testString2));
+        System.out.println(isAnagram(testString2, testString3));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>(); // create an unordered map to store char frequencies
+
+        char[] sCharArr = s.toCharArray();
+        char[] tCharArr = t.toCharArray();
+
+        for (char i : sCharArr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        for (char i : tCharArr) {
+            map.put(i, map.getOrDefault(i, 0) - 1);
+        }
+
+        for (int value : map.values()) {
+            if (value != 0) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
